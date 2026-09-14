9d3fab04f13a4a09cda78ee61fd6544e79e830f4
ItzAlma <alma.personal.hassan@gmail.com>
2026-09-14 15:01:24 +0530

    port: add scripts/installer/hypr.sh

--- scripts/installer/hypr.sh
+++ scripts/installer/hypr.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-# Get the directory of the current script
 BASE_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../")
 
-# Source helper file
-source $BASE_DIR/scripts/installer/helper.sh
+source "$BASE_DIR/scripts/installer/helper.sh"
 
 log_message "Installation started for hypr section"
 print_info "\nStarting hypr setup..."
 print_info "\nEverything is recommended to INSTALL"
 
-run_command "pacman -S --noconfirm hyprland" "Install Hyprland (Must)" "yes"
-run_command "mkdir -p /home/$SUDO_USER/.config/hypr/ && cp -r $BASE_DIR/configs/hypr/hyprland.conf /home/$SUDO_USER/.config/hypr/" "Copy hyprland config (Must)" "yes" "no" 
+refresh_sudo
 
-run_command "pacman -S --noconfirm xdg-desktop-portal-hyprland" "Install XDG desktop portal for Hyprland" "yes"
+run_command "pacman -S --noconfirm --needed hyprland" "Install Hyprland (Must)" "yes"
+run_command "mkdir -p $HOME/.config/hypr" "Create Hyprland config directory" "no" "no"
+run_command "cp -r $BASE_DIR/configs/hypr/hyprland.conf $HOME/.config/hypr/" "Copy hyprland config (Must)" "yes" "no"
+if [[ ! -f "$HOME/.config/hypr/hyprland.conf" ]]; then
+    print_error "Failed to copy hyprland.conf"
+    log_message "Failed to copy hyprland.conf"
+fi
 
-run_command "pacman -S --noconfirm polkit-kde-agent" "Install KDE Polkit agent for authentication dialogs" "yes"
+run_command "pacman -S --noconfirm --needed xdg-desktop-portal-hyprland" "Install XDG desktop portal for Hyprland" "yes"
 
-run_command "pacman -S --noconfirm dunst" "Install Dunst notification daemon" "yes"
-run_command "cp -r $BASE_DIR/configs/dunst /home/$SUDO_USER/.config/" "Copy dunst config" "yes" "no"
+run_command "pacman -S --noconfirm --needed xdg-desktop-portal-gtk" "Install XDG desktop portal GTK (for file dialogs)" "yes"
 
-run_command "pacman -S --noconfirm qt5-wayland qt6-wayland" "Install QT support on wayland" "yes"
+run_command "pacman -S --noconfirm --needed polkit-kde-agent" "Install KDE Polkit agent for authentication dialogs" "yes"
+
+run_command "pacman -S --noconfirm --needed dunst" "Install Dunst notification daemon" "yes"
+
+run_command "pacman -S --noconfirm --needed qt5-wayland qt6-wayland" "Install QT support on wayland" "yes"
 
 echo "------------------------------------------------------------------------"
